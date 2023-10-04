5338a090066e87be32a268c4497a4945cf681c53
Tom <tom.sander243@gmail.com>
2023-10-04 21:34:51 +0200

    css changes/ added chat box

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,37 +8,62 @@
 			const regex = /^(sk-|pk-)[a-zA-Z0-9]{48}$/;
 
 			if (regex.test(input!.value)) {
-				window.location.href = '/test';
+				window.location.href = '/chat';
 			}
 		});
 	});
 </script>
 
 <main>
-	<div class="main">
+	<div class="container">
 		<h1 class="text">Enter your API Key</h1>
 		<input type="password" class="input" />
 	</div>
 </main>
 
 <style>
-	.main {
+	.container {
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
-		height: 350px;
+		height: 50vh;
 	}
 
 	.input {
-		width: 22%;
-		height: 15%;
-		font-size: 25px;
+		width: 50vw;
+		max-width: 300px;
+		min-width: 150px;
+		height: 5vh;
+		font-size: 2vh;
 		box-shadow: 0 0 20px rgba(255, 255, 255, 0.3), 0 0 30px rgba(255, 255, 255, 0.2),
 			0 0 40px rgba(255, 255, 255, 0.1);
 	}
 
 	.text {
 		font-size: clamp(25px, 3vw, 50px);
+	}
+
+	@media only screen and (max-width: 600px) {
+		.container {
+			height: 70vh;
+		}
+
+		.input {
+			width: 80vw;
+			font-size: 6vh;
+		}
+
+		.text {
+			font-size: 6vw;
+		}
+	}
+
+	@media only screen and (min-width: 1080px) {
+		.input {
+			width: 30vw;
+			max-width: 500px;
+			font-size: 3vh;
+		}
 	}
 </style>
---
+++ src/routes/chat/+page.svelte
@@ -0,0 +1,98 @@
+<main>
+	<div class="chat-container">
+		<div class="chat-box" />
+		<p class="chat-message">testaapodihaldsfk hsadiufha wsieufhbasief</p>
+		<p class="chat-response">test asdfuhjkasd asdkfgasdjhf asdfasdfad asdfsf</p>
+		<div class="chat-input-container">
+			<input type="text" class="chat-input" />
+		</div>
+	</div>
+</main>
+
+<style>
+	.chat-container {
+		width: 100%;
+		max-width: 600px;
+		height: 80vh;
+		border: 1px solid #ccc;
+		margin: auto;
+		display: flex;
+		flex-direction: column;
+		margin-top: 2vw;
+	}
+
+	.chat-box {
+		flex: 1;
+		overflow-y: auto;
+		padding: 2vw;
+		font-family: 'IBM Plex Mono', monospace;
+	}
+
+	.chat-input-container {
+		border-top: 1px solid #ccc;
+		padding: 1vw;
+	}
+
+	.chat-input {
+		width: 100%;
+		padding: 1vw;
+		box-sizing: border-box;
+		box-shadow: 0 0 20px rgba(255, 255, 255, 0.3), 0 0 30px rgba(255, 255, 255, 0.2),
+			0 0 40px rgba(255, 255, 255, 0.1);
+		font-family: 'IBM Plex Mono', monospace;
+	}
+
+	.chat-message {
+		font-size: 1.5vw;
+		border: 1px solid #ccc;
+		width: fit-content;
+		padding: 0.3vw;
+		padding-left: 1vw;
+		margin-left: auto;
+		margin-right: 1vw;
+		word-wrap: break-word;
+		overflow-wrap: break-word;
+		box-sizing: border-box;
+	}
+
+	.chat-message::first-line {
+		text-align: right;
+	}
+
+	.chat-response {
+		font-size: 1.5vw;
+		border: 1px solid #ccc;
+		width: fit-content;
+		padding: 0.3vw;
+		margin-left: 1vw;
+		margin-right: auto;
+		word-wrap: break-word;
+		overflow-wrap: break-word;
+	}
+
+	@media only screen and (max-width: 600px) {
+		.chat-container {
+			height: 70vh;
+			max-width: 400px;
+		}
+
+		.chat-box {
+			padding: 4vw;
+		}
+
+		.chat-input-container {
+			padding: 2vw;
+		}
+
+		.chat-input {
+			padding: 2vw;
+		}
+	}
+
+	@media only screen and (min-width: 2000px) {
+		.chat-container {
+			height: 80vh;
+			max-width: 800px;
+		}
+	}
+</style>
